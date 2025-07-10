2583cf4b377bfe4a00bdfe4618976e9f38c9e2fd
viktoria114 <90705329+viktoria114@users.noreply.github.com>
2025-07-10 19:27:59 -0300

    inicializacion frontend

---
+++ DICA-APP/dica-app/src/App.tsx
@@ -0,0 +1,18 @@
+import { Typography } from '@mui/material';
+import './App.css'
+import Box from '@mui/material/Box';
+
+
+function App() {
+
+  return (
+    <>
+      <Box bgcolor={'white'} p={5} boxShadow={2} borderRadius={3}>
+       <Typography color='black'>Bienvenido a Dica!</Typography>
+      </Box>
+   
+    </>
+  )
+}
+
+export default App
