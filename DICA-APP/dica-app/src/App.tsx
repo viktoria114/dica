8386b3e3b7c28db9c377d1cc135a1a9c16506ea6
viktoria114 <90705329+viktoria114@users.noreply.github.com>
2025-07-11 17:45:14 -0300

    theme in progress

--- DICA-APP/dica-app/src/App.tsx
+++ DICA-APP/dica-app/src/App.tsx
@@ -1,17 +1,69 @@
-import { Typography } from '@mui/material';
+import { Button, TextField, ThemeProvider, Typography } from '@mui/material';
 import './App.css'
 import Box from '@mui/material/Box';
+import theme from './services/theme';
 
 
 function App() {
 
   return (
-    <>
-      <Box bgcolor={'white'} p={5} boxShadow={2} borderRadius={3}>
-       <Typography color='black'>Bienvenido a Dica!</Typography>
-      </Box>
-   
-    </>
+  
+    <ThemeProvider theme={theme}>
+      <Box
+          component="form"
+          bgcolor="primary.main"
+          p={5}
+          boxShadow={2}
+          borderRadius={3}
+        >
+          <Typography fontWeight={550} color="text.secondary">
+            Inicio de sesión: Dica
+          </Typography>
+          <Box m={2} display="flex" flexDirection="column" gap={2}>
+            <TextField
+              id="username"
+              label="Usuario o correo electrónico"
+              fullWidth
+               sx={{
+                backgroundColor: 'background.default',
+                borderRadius: 2,
+              }}
+            />
+            <TextField
+              id="password"
+              label="Contraseña"
+              type="password"
+              fullWidth
+               sx={{
+                backgroundColor: 'background.default',
+                borderRadius: 2,
+              }}
+               InputLabelProps={{
+    sx: {
+      color: 'primary.main',
+    },
+  }}
+  inputProps={{
+    style: {
+      color: theme.palette.primary.main,
+    },
+  }}
+            />
+            <Button
+            fullWidth sx={{
+                backgroundColor: 'background.default',
+                color: 'primary.main',
+                
+                '&:hover': {
+                  backgroundColor: '#e0e0e0',
+                },
+              }}
+            >Ingresar</Button>
+          </Box>
+        </Box>
+      
+   </ThemeProvider>
+    
   )
 }
 
