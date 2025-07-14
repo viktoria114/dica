0bcd173a54b152c4bf6eb57bb3a791ba78cbf3cf
MatiasMXS <130255970+MatiasMXS@users.noreply.github.com>
2025-07-14 16:47:52 -0300

    Login y middleware creados

--- API-DICA/src/middlewares/authHandler.ts
+++ API-DICA/src/middlewares/authHandler.ts
@@ -3,8 +3,9 @@ import jwt from "jsonwebtoken";
 
 // extendemos Request para incluir los datos del usuario
 interface AuthenticatedRequest extends Request {
-  DNI?: number;
+  dni?: number;
   rol?: string;
+  username?: string;
 }
 
 export const verifyToken = (req: AuthenticatedRequest, res: Response, next: NextFunction): void => {
@@ -19,12 +20,22 @@ export const verifyToken = (req: AuthenticatedRequest, res: Response, next: Next
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
-      DNI: number;
-      rol: string;
-    };
+    dni: number;
+    rol: string;
+    username: string;
+  };
 
-    req.DNI = decoded.DNI;
+  console.log("✅ TOKEN DECODIFICADO:", decoded);
+
+    req.dni = decoded.dni;
     req.rol = decoded.rol;
+    req.username = decoded.username;
+
+    console.log("🔍 VALORES EN req:", {
+  dni: req.dni,
+  rol: req.rol,
+  username: req.username
+});
 
     next();
   } catch (error) {
--- API-DICA/src/routes/empleadoRoutes.ts
+++ API-DICA/src/routes/empleadoRoutes.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
 import { crearEmpleado, getEmpleados, actualizarEmpleado } from '../controllers/empleadoController';
-import { checkRole } from '../middlewares/authHandler';
+import {verifyToken, checkRole } from '../middlewares/authHandler';
 const router = Router();
 
 //aqui se importan los Controllers para cada ruta
 
-router.get('/', /*checkRole(['admin']), */ getEmpleados); //getEmpleados
+router.get('/', verifyToken,checkRole(['admin']),getEmpleados); //getEmpleados
 //router.get('/:id', ); //getEmpleadoByID
 router.post('/', crearEmpleado);//createEmpleado
 router.put('/:id', actualizarEmpleado); // actualizarEmpleado
