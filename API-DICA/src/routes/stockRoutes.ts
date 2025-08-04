95fa99ac5d0efbe3c184ebf325d5e508cbc08d16
MrPointed <salomon_98_2@hotmail.es>
2025-08-04 17:42:51 -0300

    added some register-stock CRUD ops

--- API-DICA/src/routes/stockRoutes.ts
+++ API-DICA/src/routes/stockRoutes.ts
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { verifyToken,checkRole } from "../middlewares/authHandler";
-import { crearStock, getStockNoVisible, getStockVisible, validateLowStock } from "../controllers/stockControllers";
+import { CrearRegistroStock, crearStock, eliminarRegistroStock, GetRegistrosStock, getStockNoVisible, getStockVisible, validateLowStock } from "../controllers/stockControllers";
 const router = Router();
 
 router.post('/', crearStock);  //PostStock
 router.get('/', getStockVisible) //GetStock
 router.get('/invisibles', getStockNoVisible) //GetStock
 router.get('/verificar', validateLowStock)
+
+router.post('/registro/:id', CrearRegistroStock) //post RegistroStock
+router.get('/registro/:id', GetRegistrosStock)
+router.delete('/registro/:id', eliminarRegistroStock)
 
 export default router;
