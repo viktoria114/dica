acd38bcffab7b5aa061178d36cb47b5be2f3efbe
viktoria114 <90705329+viktoria114@users.noreply.github.com>
2025-08-05 16:54:07 -0300

    empleados en proceso

--- API-DICA/src/app.ts
+++ API-DICA/src/app.ts
@@ -2,25 +2,25 @@ import express from 'express';
 import empleadoRoutes from './routes/empleadoRoutes';
 import clienteRoutes from './routes/clienteRoutes';
 import sugerenciaRoutes from './routes/sugerenciaRoutes'
-import stockRoutes from './routes/stockRoutes'
 import authRoutes from "./routes/authRoutes";
 import agenteRoutes from './routes/agenteRoutes'
-import menuRoutes from './routes/menuRoutes'
 import { errorHandler } from './middlewares/errorHandler';
 import cors from 'cors'
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true,
+  exposedHeaders: ["Authorization"], 
+}));
 // Routes
 app.use('/api/empleados', empleadoRoutes);
 app.use('/api/clientes', clienteRoutes);
 app.use('/api/sugerencias', sugerenciaRoutes)
-app.use('/api/stock', stockRoutes)
 app.use("/api/auth", authRoutes);
 app.use('/api/agente', agenteRoutes);
-app.use('/api/menu', menuRoutes)
 
 app.get('/', (req, res) => {
     res.send('¡Bienvenido a mi API: DICA');
