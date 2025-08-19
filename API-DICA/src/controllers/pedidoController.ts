e9d9966edc56ea3915c64e11a1352f2889152ab1
MatiasMXS <matifmorenoxd@gmail.com>
2025-08-19 16:11:14 -0300

    added agregarItemPedido, eliminarItemsPedido and getItemPedido

--- API-DICA/src/controllers/pedidoController.ts
+++ API-DICA/src/controllers/pedidoController.ts
@@ -5,70 +5,70 @@ import { Pedido } from '../models/pedido';
 import { MessagePort } from 'worker_threads';
 
 export const crearPedido = async (req: Request, res: Response) => {
-  const client: PoolClient = await pool.connect();
-  try {
-    const { fk_empleado, fk_cliente, ubicacion, observacion, items_menu } =
-      req.body;
+    const client: PoolClient = await pool.connect();
+    try {
+        const { fk_empleado, fk_cliente, ubicacion, observacion, items_menu } =
+            req.body;
 
-    await client.query('BEGIN');
+        await client.query('BEGIN');
 
-    const pedido = new Pedido(
-      null,
-      null,
-      null,
-      null,
-      fk_empleado,
-      fk_cliente,
-      ubicacion,
-      observacion,
-      true,
-    );
+        const pedido = new Pedido(
+            null,
+            null,
+            null,
+            null,
+            fk_empleado,
+            fk_cliente,
+            ubicacion,
+            observacion,
+            true,
+        );
 
-    // Insertar el pedido
-    const pedidoQuery = `
+        // Insertar el pedido
+        const pedidoQuery = `
             INSERT INTO pedidos (fecha, hora, estado, dni_empleado, id_cliente, ubicacion, observaciones, visibilidad)
             VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
             RETURNING id;
         `;
-    const { rows: pedidoRows } = await client.query(pedidoQuery, [
-      new Date(),
-      pedido.hora,
-      pedido.estado,
-      pedido.fk_empleado,
-      pedido.fk_cliente,
-      pedido.ubicacion,
-      pedido.observacion,
-      pedido.visibilidad
-    ]);
+        const { rows: pedidoRows } = await client.query(pedidoQuery, [
+            new Date(),
+            pedido.hora,
+            pedido.estado,
+            pedido.fk_empleado,
+            pedido.fk_cliente,
+            pedido.ubicacion,
+            pedido.observacion,
+            pedido.visibilidad
+        ]);
 
-    const pedidoId = pedidoRows[0].id;
+        const pedidoId = pedidoRows[0].id;
 
-    // Insertar en pedidos_menu
-    const pedido_menuQuery = `
+        // Insertar en pedidos_menu
+        const pedido_menuQuery = `
             INSERT INTO pedidos_menu (fk_pedido, fk_menu, precio_unitario)
             VALUES ($1, $2, $3);
         `;
 
-    for (const item of items_menu) {
-      const result = await client.query(
-        'SELECT precio FROM menu WHERE id = $1',
-        [item.id_menu],
-      );
-      const precio = result.rows[0].precio;
+        for (const item of items_menu) {
+            const result = await client.query(
+                'SELECT precio FROM menu WHERE id = $1',
+                [item.id_menu],
+            );
+            const precio = result.rows[0].precio;
 
-      await client.query(pedido_menuQuery, [pedidoId, item.id_menu, precio]);
+            await client.query(pedido_menuQuery, [pedidoId, item.id_menu, precio]);
+        }
+
+        await client.query('COMMIT');
+
+        res.status(201).json({ id: pedidoId, message: 'Pedido creado con éxito' });
+    } catch (error) {
+        await client.query('ROLLBACK');
+        console.error(error);
+        res.status(500).json({ message: 'Error al crear el pedido' });
+    } finally {
+        client.release();
     }
-
-    await client.query('COMMIT');
-
-    res.status(201).json({ id: pedidoId, message: 'Pedido creado con éxito' });
-  } catch (error) {
-    await client.query('ROLLBACK');
-    console.error(error);
-    res.status(500).json({ message: 'Error al crear el pedido' });
-  } finally {
-    client.release();
-  }
 };
 
 export const actualizarPedido = async (req: Request, res: Response) => {
@@ -84,24 +84,24 @@ export const actualizarPedido = async (req: Request, res: Response) => {
         `;
 
         const pedido = new Pedido(
-      null,
-      null,
-      null,
-      estado,
-      fk_empleado,
-      fk_cliente,
-      ubicacion,
-      observacion,
-    );
+            null,
+            null,
+            null,
+            estado,
+            fk_empleado,
+            fk_cliente,
+            ubicacion,
+            observacion,
+        );
 
         const { rows } = await pool.query(query, [
-      pedido.estado,
-      pedido.fk_empleado,
-      pedido.fk_cliente,
-      pedido.ubicacion,
-      pedido.observacion,
+            pedido.estado,
+            pedido.fk_empleado,
+            pedido.fk_cliente,
+            pedido.ubicacion,
+            pedido.observacion,
 
-      id
+            id
         ]);
         if (rows.length === 0) {
             return res.status(404).json({ message: "Pedido no encontrado" });
@@ -182,3 +182,84 @@ export const restaurarPedido = async (req: Request, res: Response) => {
         res.status(500).json({ message: "Error al restaurar el Pedido" });
     }
 };
+
+//Logica de negocio
+
+export const agregarItemPedido = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params;
+        const { items_menu } =
+            req.body;
+
+        const pedidoId = id;
+
+        // Insertar en pedidos_menu
+        const pedido_menuQuery = `
+            INSERT INTO pedidos_menu (fk_pedido, fk_menu, precio_unitario)
+            VALUES ($1, $2, $3);
+        `;
+
+        for (const item of items_menu) {
+            const result = await pool.query(
+                'SELECT precio FROM menu WHERE id = $1',
+                [item.id_menu],
+            );
+            const precio = result.rows[0].precio;
+
+            await pool.query(pedido_menuQuery, [pedidoId, item.id_menu, precio]);
+        }
+
+
+        res.status(201).json({ id: pedidoId, message: 'Items agregados con éxito' });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: 'Error al agregar Items el pedido' });
+    };
+}
+
+export const eliminarItemsPedido = async (req: Request, res: Response) => {
+    try {
+        const { ids } = req.body; // Array con los ids de los items a eliminar
+
+        if (!Array.isArray(ids) || ids.length === 0) {
+            return res.status(400).json({ message: "Debes enviar un array con los IDs a eliminar" });
+        }
+
+        // Generamos placeholders ($1, $2, $3, ...)
+        const placeholders = ids.map((_, index) => `$${index + 1}`).join(", ");
+
+        const query = `
+            DELETE FROM pedidos_menu
+            WHERE id IN (${placeholders})
+            RETURNING *;
+        `;
+
+        const { rows } = await pool.query(query, ids);
+
+        if (rows.length === 0) {
+            return res.status(404).json({ message: "No se encontraron items con esos IDs" });
+        }
+
+        res.json({ message: "Items eliminados correctamente", eliminados: rows });
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error al eliminar los items" });
+    }
+};
+
+export const getItemPedido = async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+        
+        const query = `SELECT * FROM pedidos_menu WHERE fk_pedido= $1 ORDER BY id ASC;`;
+        const { rows } = await pool.query(query, [id]);
+        res.json(rows);
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ message: "Error al obtener los Pedidos visible" });
+    }
+}
+
+
+//router.get('/:id', mostrarItemPedido)
+//router.delete('/:id', eliminarItemPedido)
--- API-DICA/src/routes/pedidoRoutes.ts
+++ API-DICA/src/routes/pedidoRoutes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import {crearPedido, actualizarPedido, getListaPedidos,eliminarPedido, getListaCompletaPedidos, restaurarPedido} from "../controllers/pedidoController"
+import { crearPedido, actualizarPedido, getListaPedidos, eliminarPedido, getListaCompletaPedidos, restaurarPedido, agregarItemPedido, eliminarItemsPedido, getItemPedido } from "../controllers/pedidoController"
 const router = Router()
 
 router.post('/', crearPedido)
@@ -8,5 +8,18 @@ router.get('/', getListaPedidos)
 router.get('/invisibles', getListaCompletaPedidos)
 router.delete('/:id', eliminarPedido)
 router.put('/restaurar/:id', restaurarPedido)
+
+//Logica de negocio
+router.post('/item/:id', agregarItemPedido)
+router.delete('//item/:id', eliminarItemsPedido)
+router.get('/item/:id', getItemPedido)
+//router.put('/cancelar/:id', cancelarPedido)
+//router.put('/estado/:id', actualizarEstadoPedido)
+//router.put('/estado/:id', retrocederEstadoPedido)
+//router.put('/estado/:id', agenteInicioPedido)
+//router.put('/estado/:id', agenteCandelarPedido)
+//router.put('/estado/:id', pedidoPagado)
+//router.put('/estado/:id', pedidoRenbolsado)
+
 
 export default router;
