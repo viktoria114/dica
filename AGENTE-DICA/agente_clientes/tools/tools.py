5356850d7330df21dac9ec0d5f8cf62f30fcd96e
MrPointed <salomon_98_2@hotmail.es>
2025-08-28 11:40:39 -0300

    added base tools for orders, it need improvements

--- AGENTE-DICA/agente_clientes/tools/tools.py
+++ AGENTE-DICA/agente_clientes/tools/tools.py
@@ -7,13 +7,11 @@ api_url = os.getenv("API_DICA_URL", "http://localhost:3000")
 
 logger = logging.getLogger(__name__)
 
-def get_customer_information(tel: int) -> str:
+def get_customer_information(tel: str) -> str:
     """
-    Obtiene informacion sobre un cliente basado en el numero de telefono a una base de datos
-
+    Obtiene informacion sobre un cliente desde una base de datos
     Args:
-        tel(str): El numero de telefono necesario para realizar la busqueda.
-
+        tel(str): Numero de telefono del cliente
     Returns:
         str: Un string de json con la informacion obtenida de la base de datos.
 
@@ -33,7 +31,7 @@ def get_customer_information(tel: int) -> str:
         print(f"Error inesperado: {e}")
         return "error inesperado al obtener cliente por telefono"
 
-def update_customer_name_and_diet(tel: int, nombre: str, dieta: str) -> str:
+def update_customer_name_and_diet(tel: str, nombre: str, dieta: str) -> str:
     """
     Actualiza el nombre y el tipo de dieta sobre un cliente usando su numero de telefono en la base de datos
     Las dietas disponibles son: vegetariano, vegano, neutra y sin asignar
@@ -165,10 +163,10 @@ def get_menu():
         print(f"Error inesperado: {e}")
         return "error inesperado al intentar crear la sugerencia"
 
-def send_menu_image(tel: str):
+def send_menu_image(tel: str) -> str:
     """
     Envia una imagen con el menu y sus precios
-    Args: 
+    Args:
         tel(str): Numero de telefono del cliente
     Returns:
         Mensaje de respuesta del estado del envio
@@ -190,4 +188,213 @@ def send_menu_image(tel: str):
         print(f"Error inesperado: {e}")
         return "error inesperado al intentar enviar la lista del menu"
 
- 
+#Crear un nuevo pedido en estado "En construccion"
+#API: business rule, no puede haber mas de dos pedidos en estado "En construccion"
+def create_new_cart(tel: str)->str:
+    """
+    Crea un nuevo carrito para agregar productos de menu
+
+    Args:
+        tel(str): Numero de telefono del cliente
+    Returns:
+        Un string con la informacion sobre la solicitud de creacion del nuevo carrito 
+    """
+
+    create_cart_url = f"{api_url}/api/pedido"
+
+
+    body = {
+        "fk_cliente": tel, 
+    }
+
+    try:
+        resultado = solicitud_con_token(create_cart_url, "POST", body)
+        return resultado
+    except requests.RequestException as e:
+        print(f"Error al crear el carrito: {e}")
+        return f"Error al crear el carrito: {e}"
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        return "error inesperado al intentar crear el carrito"
+
+
+#API: business rule, no puede haber mas de dos pedidos en estado "En construccion"
+def get_active_cart(tel: str):
+    """
+    Obtiene informacion sobre el carrito activo del cliente
+    Args:
+        tel(str): Numero de telefono del cliente
+    Returns: 
+        informacion sobre el estado o existencia del carrito activo del cliente
+    """
+
+    get_cart_url = f"{api_url}/api/pedido/{tel}"
+
+    try:
+        resultado = solicitud_con_token(get_cart_url, "GET")
+        return resultado
+    except requests.RequestException as e:
+        print(f"Error al obtener el carrito activo: {e}")
+        return f"Error al obtener el carrito activo: {e}"
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        return "error inesperado al intentar obtener el carrito activo"
+
+
+def add_menu_to_cart(tel: str, menuID: int, cantidad: int):
+    """
+    Agrega un item de menu al carrito
+
+    Args:
+        tel(str): Numero de telefono del cliente
+        menuID(int): ID del menu que se desea agregar
+        cantidad(int): cantidad de items de menu
+    Returns: 
+        Estado de la peticion y del carrito actualizado
+    """
+
+    add_menu_url = f"{api_url}/api/pedido/items/{tel}"
+
+    body = {
+        "fk_menu": menuID,
+        "cantidad": cantidad
+    }
+
+    try:
+        resultado = solicitud_con_token(add_menu_url, "POST", body)
+        return resultado
+    except requests.RequestException as e:
+        print(f"Error al agregar el menu al carrito: {e}")
+        return f"Error al agregar el menu al carrito: {e}"
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        return "error inesperado al intentar agregar el menu al carrito"
+
+def remove_menu_from_cart(tel: str, menuID: int, cantidad: int) ->str:
+    """
+    Quita un item de menu del carrito
+
+    Args:
+        tel(str): Numero de telefono del cliente
+        menuID(int): ID del menu que se desea quitar
+        cantidad(int): cantidad de items de menu
+    Returns: 
+        Estado del carrito actualizado
+    """
+
+    body = {
+        "fk_menu": menuID,
+        "cantidad": cantidad
+    }
+
+    remove_menu_url= f"{api_url}/api/pedido/cart/{tel}"
+
+    try:
+        resultado = solicitud_con_token(remove_menu_url, "DELETE", body)
+        return resultado
+    except requests.RequestException as e:
+        print(f"Error al quitar el menu del carrito: {e}")
+        return f"Error al quitar el menu del carrito: {e}"
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        return "error inesperado al intentar quitar el menu del carrito"
+
+
+# Esta funcion en realidad actualiza el pedido ya creado pero el agente no lo sabe jiji
+def create_order(tel: str, ubicacion: str, observacion: str):
+    """
+    Crea un nuevo pedido para ser procesado
+
+    Args:
+        tel(str): Numero de telefono del cliente
+        ubicacion(str): Lugar donde entregar el pedido
+        observacions(str): detalles extra sobre el pedido especificadas (preferencias, referencias del lugar de entrega, etc)
+    Returns:
+        Informacion sobre el pedido creado
+    """
+    body = {
+        "ubicacion": ubicacion,
+        "observacion": observacion
+    }
+    # API: Buscar el unico pedido que corresponde al cliente y que tenga el estado "En construccion"
+    create_order_url= f"{api_url}/api/pedido/{tel}"
+
+    try:
+        resultado = solicitud_con_token(create_order_url, "POST", body)
+        return resultado
+    except requests.RequestException as e:
+        print(f"Error al crear el pedido: {e}")
+        return f"Error al crear el pedido: {e}"
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        return "error inesperado al intentar crear el pedido"
+
+
+def cancel_order(tel: str, orderID: int):
+    """
+    Cancela una order activa del cliente
+    """
+
+    body = {
+        "telefono": tel
+    }
+
+    cancel_order_url= f"{api_url}/api/pedido/cancelar/{orderID}"
+
+    try:
+        resultado = solicitud_con_token(cancel_order_url, "POST", body)
+        return resultado
+    except requests.RequestException as e:
+        print(f"Error al cancelar el pedido: {e}")
+        return f"Error al cancelar el pedido: {e}"
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        return "error inesperado al intentar cancelar el pedido"
+
+# vacia los items del pedido "En construccion"
+def cancel_cart(tel: str):
+    """
+    Vacia el carrito para futuras modificaciones
+    Args:
+        tel(str): numero de telefono del cliente
+    Returns:
+        string: resultado de la operacion
+    """
+
+    body = {
+        "telefono": tel
+    }
+
+    #API: vaciar el pedido donde num = tel y estado = "En construccion"
+    cancel_order_url= f"{api_url}/api/pedido/cancelar/{tel}"
+
+    try:
+        resultado = solicitud_con_token(cancel_order_url, "POST", body)
+        return resultado
+    except requests.RequestException as e:
+        print(f"Error al cancelar el carrito: {e}")
+        return f"Error al cancelar el carrito: {e}"
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        return "error inesperado al intentar cancelar el carrito"
+
+
+#permite ver todas las ordenes no canceladas/finalizadas asociadas al cliente
+def check_active_orders(tel: str):
+    """
+    Informa sobre el estado de las ordenes activas del cliente
+    """
+
+    #API: vaciar el pedido donde num = tel y estado = "En construccion"
+    check_orders_url= f"{api_url}/api/pedido/{tel}"
+
+    try:
+        resultado = solicitud_con_token(check_orders_url, "GET")
+        return resultado
+    except requests.RequestException as e:
+        print(f"Error al revisar las ordenes del cliente: {e}")
+        return f"Error al revisar las ordenes del cliente: {e}"
+    except Exception as e:
+        print(f"Error inesperado: {e}")
+        return "error inesperado al revisar las ordenes del cliente"
+
