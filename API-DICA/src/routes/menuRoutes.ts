a155909a708e7932e9c04b172a450dff8a804a16
MrPointed <salomon_98_2@hotmail.es>
2025-09-17 17:43:20 -0300

    added get menu details endpoint

--- API-DICA/src/routes/menuRoutes.ts
+++ API-DICA/src/routes/menuRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express"
-import {crearMenu, actualizarMenu, getListaMenu, getListaInvisibleMenu, eliminarMenu, restaurarMenu, getMenuImage} from "../controllers/menuController"
+import {crearMenu, actualizarMenu, getMenuDetalles, getListaMenu, getListaInvisibleMenu, eliminarMenu, restaurarMenu, getMenuImage} from "../controllers/menuController"
 const router = Router()
 
 router.post('/', crearMenu)
 router.put('/:id', actualizarMenu)
+router.get('/:id', getMenuDetalles)
 router.get('/', getListaMenu)
 router.get('/invisibles', getListaInvisibleMenu)
 router.delete('/:id', eliminarMenu)
