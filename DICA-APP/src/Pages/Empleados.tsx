dd13d888e1b8c27861dce71e7b495a90e6d2489a
viktoria114 <90705329+viktoria114@users.noreply.github.com>
2025-09-19 23:22:38 -0300

    reordenamiento y cambio de nombre

--- DICA-APP/src/Pages/Empleados.tsx
+++ DICA-APP/src/Pages/Empleados.tsx
@@ -1,17 +1,103 @@
-import { ListaEmpleados } from "../Components/Empleados/ListaEmpleados";
+import { Box, Container, LinearProgress } from "@mui/material";
+import { useCallback, useState } from "react";
+import { useEmpleados } from "../hooks/useEmpleados";
+import type { Empleado } from "../types";
+import { SearchBar } from "../Components/common/SearchBar";
+import { FichaEmpleado } from "../Components/Empleados/FichaEmpleado";
+import { ModalBase } from "../Components/common/ModalBase";
+import EmpleadoForm from "../Components/Empleados/FormEmpleado";
+import { Paginacion } from "../Components/common/Paginacion";
+
+const styleBox1 = {
+  bgcolor: "primary.main",
+  boxShadow: 1,
+  borderRadius: 2,
+  p: 2,
+  minWidth: 300,
+  display: "grid",
+  gridTemplateColumns: { sm: "repeat(3, 1fr)", xs: "repeat(1, 1fr)" },
+  gap: 2,
+  justifyContent: "center",
+  mb: 2,
+};
+
 export const Empleados = () => {
+ const { empleados, loading, error, modoPapelera, toggleInvisibles } = useEmpleados();
+ const rowsPerPageOptions:number[] = [9, 15, 21]
+
+  // ✅ Estado para búsqueda
+  const [empleadosMostrados, setEmpleadosMostrados] = useState<Empleado[]>([]);
+  const getLabel = useCallback((e: Empleado) => e.nombre_completo, []);
+
+  const [page, setPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(9);
+
+  // ✅ Estado para modal de creación
+  const [showForm, setShowForm] = useState(false);
+  const handleAdd = useCallback(() => setShowForm(true), []);
+
+  // ✅ si hay búsqueda, mostramos resultados; si no, usamos los del hook
+  const listaParaRenderizar =
+    empleadosMostrados.length > 0 ? empleadosMostrados : empleados;
+
+      const paginatedEmpleados = listaParaRenderizar.slice(
+    page * rowsPerPage,
+    page * rowsPerPage + rowsPerPage
+  );
   return (
-      <div
-      /*style={{
-    backgroundImage: 'url("Frame 1.png")',
-    backgroundRepeat: 'repeat', 
-    width: '100%',
-    height: '300px',
-  }}
-    */
-      >
-        <ListaEmpleados></ListaEmpleados>
-       
-      </div>
+    <>
+      {loading && <LinearProgress color="inherit" />}
+      {error && <p>Error: {error}</p>}
+      <Container>
+        <SearchBar<Empleado>
+          baseList={empleados}
+          getLabel={getLabel}
+          placeholder={"Buscar empleados por nombre..."}
+          onResults={setEmpleadosMostrados}
+          onAdd={handleAdd}
+          onShowInvisibles={toggleInvisibles}
+          disableAdd={modoPapelera}
+          papeleraLabel={modoPapelera ? "Volver" : "Papelera"}
+        />
+
+        <Box sx={styleBox1}>
+          {paginatedEmpleados.map((empleado) => (
+            <FichaEmpleado
+              key={empleado.dni}
+              empleado={empleado}
+              modoPapelera={modoPapelera}
+            />
+          ))}
+        </Box>
+         <Paginacion
+                  page={page}
+                  setPage={setPage}
+                  rowsPerPage={rowsPerPage}
+                  setRowsPerPage={setRowsPerPage}
+                  count={listaParaRenderizar.length}
+                  rowsPerPageOptions = {rowsPerPageOptions}
+                />
+      </Container>
+
+      <ModalBase open={showForm} onClose={() => setShowForm(false)}>
+        <EmpleadoForm
+          modo="crear"
+          initialValues={{
+            nombre_completo: "",
+            username: "",
+            password: "",
+            correo: "",
+            telefono: "",
+            rol: "cajero", // valor por defecto
+            dni: "", // si es requerido por el tipo Empleado
+          }}
+          onSuccess={() => {
+            // refetchEmpleados?.();
+            setShowForm(false);
+          }}
+          onCancel={() => setShowForm(false)}
+        />
+      </ModalBase>
+    </>
   );
 };
