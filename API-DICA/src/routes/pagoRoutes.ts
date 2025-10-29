765649ddde6e2a1a781f91ce2c6de673daae5f83
MrPointed <salomon_98_2@hotmail.es>
2025-10-29 20:49:57 -0300

    added delete pago endpoint

--- API-DICA/src/routes/pagoRoutes.ts
+++ API-DICA/src/routes/pagoRoutes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { verifyToken, checkRole } from "../middlewares/authHandler";
-import { crearPago, actualizarPago, getPagos, getPagoPorId } from "../controllers/pagoController";
+import { crearPago, actualizarPago, getPagos, getPagoPorId, eliminarPagos } from "../controllers/pagoController";
 import { sendDropBoxToken } from "../utils/image";
 
 const router = Router();
@@ -9,6 +9,7 @@ router.post('/', crearPago); //PostPago
 router.get('/', getPagos); //GetPagos
 router.get('/token', sendDropBoxToken) //obtiene el token necesario para ver comprobantes desde el front
 router.get('/:id', getPagoPorId); //GetPago Por ID
+router.delete('/:id', eliminarPagos); //DeletePago
 router.put('/:id', actualizarPago); //PutStock - Actualizar
 
 export default router;
