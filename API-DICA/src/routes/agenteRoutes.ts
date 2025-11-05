efaee3495d84938147f471e495e7e58c6c9a73b1
MrPointed <salomon_98_2@hotmail.es>
2025-11-04 22:28:40 -0300

    cleaned inicio page and added button for toggle agent

--- API-DICA/src/routes/agenteRoutes.ts
+++ API-DICA/src/routes/agenteRoutes.ts
@@ -1,9 +1,10 @@
 import {Router} from 'express';
-import { gestionarMensajes, gestionarVerificacion, toggleActivity} from '../controllers/agenteController';
+import { gestionarMensajes, gestionarVerificacion, toggleActivity, getAgentStatus} from '../controllers/agenteController';
 
 const router = Router()
 
-router.post('/activity/toggle', toggleActivity)
+router.post('/toggle-activity', toggleActivity)
+router.get('/status', getAgentStatus)
 router.get('/', gestionarVerificacion)
 router.post('/', gestionarMensajes)
 
